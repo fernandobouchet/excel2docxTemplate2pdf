de2f64cc174558a015e448aa0dd7129915ba2d79
fernandobouchet <fernandobouchet@gmail.com>
2024-07-05 20:12:02 -0300

    feat: add 'themeToggle' component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import { ThemeToggle } from "@/components/themeToggle";
+
 export default function Home() {
   return (
     <main>
-      <h1>Hello Nextjs!</h1>
+      <ThemeToggle />
     </main>
   );
 }
