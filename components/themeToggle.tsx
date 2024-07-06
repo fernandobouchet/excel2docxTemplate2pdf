4b6e0cb45e79d866e96c2af7d6fb0f3a9eeb98fa
fernandobouchet <fernandobouchet@gmail.com>
2024-07-06 15:32:40 -0300

    feat: update 'Home' and 'themeToggle' components styles

--- components/themeToggle.tsx
+++ components/themeToggle.tsx
@@ -5,7 +5,11 @@ import { useTheme } from "next-themes";
 
 import { Button } from "@/components/ui/button";
 
-export function ThemeToggle() {
+interface Props extends React.ComponentPropsWithoutRef<"button"> {
+  className?: string;
+}
+
+export function ThemeToggle({ className }: Props) {
   const { theme, setTheme } = useTheme();
 
   const handleOnClick = () => {
@@ -13,7 +17,12 @@ export function ThemeToggle() {
   };
 
   return (
-    <Button variant="ghost" size="icon" onClick={handleOnClick}>
+    <Button
+      variant="ghost"
+      size="icon"
+      className={className}
+      onClick={handleOnClick}
+    >
       <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
       <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
       <span className="sr-only">Toggle theme</span>
